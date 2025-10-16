6ba137ad2e9a2322d4319008a5f2acee9e09b50a
fkhantahseen <tahseenkhanfatima@gmail.com>
2025-10-16 20:32:46 +0530

    Update exp1A.c

--- exp1A.c
+++ exp1A.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
-  printf("Name: Tahseen\n");
-printf("Age: 17\n");
-printf("Class: Comp2\n");
+  printf("Name:Tahseen\n");
+printf("Age:17\n");
+printf("Class:Comp2\n");
 printf("Division:D\n");
-printf("UIN:251P116");
+printf("UIN:251P116\n");
 
 return 0;
 }
