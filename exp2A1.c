d63a4d69f0ae1d2d0c9cc3e2e21b00047ff3f47d
fkhantahseen <tahseenkhanfatima@gmail.com>
2025-10-16 15:37:54 +0530

    Create exp2A1.c

---
+++ exp2A1.c
@@ -0,0 +1,19 @@
+/* Name: Tahseen
+UIN: 251P116
+Division: D
+Class : Computer
+*/
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter the number:");
+    scanf("%d", &num);
+    if(num % 2 == 0){
+        printf("number is even");
+    
+    }
+    else {
+        printf("number is odd");
+    }
+    return 0;
+}
