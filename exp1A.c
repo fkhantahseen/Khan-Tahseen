30d36483abc881b920e2a229142978fa6ce7041c
fkhantahseen <tahseenkhanfatima@gmail.com>
2025-10-16 15:55:02 +0530

    Create exp1A.c

---
+++ exp1A.c
@@ -0,0 +1,24 @@
+/* Name: Tahseen
+UIN: 251P116
+Division: D
+Class : Computer
+*/
+#include<stdio.h>
+int main(){
+int P; 
+int C; 
+int M;
+printf("Enter a Physics Marks:");
+scanf("%d",&P);
+printf("Enter a Chemistry Marks:");
+scanf(%d",&C);
+printf("Enter a Maths Marks:"):
+scanf("&d",&M0;
+(P+C=M % 3 == 50) ? printf("Student is eligible") :  printf("Student is not eligible");
+return 0; 
+}
+
+
+
+
+
