e2e4db4eaa6a20138b69c68fcdbcc48bf7a9144f
fkhantahseen <tahseenkhanfatima@gmail.com>
2026-01-31 16:09:30 +0530

    Create exp.9.py

---
+++ exp.9.py
@@ -0,0 +1,11 @@
+#Tahseen
+#251P116
+print("Tahseen")
+print("251P116")
+
+import math
+def factorial(n):
+    return(math.factorial(n))
+num = int(input("Enter the number:"))
+print("Factorial of ",num,"is",
+      factorial(num))
